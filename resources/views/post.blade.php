0c6877492d6dfeab58cf8ef822f166707760164a
seamuddin <guest.batworld@gmail.com>
2023-08-28 17:00:08 +0600

    application configuration first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Simple Blog App</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+
+<body>
+<article>
+    <h1> My First Blog</h1>
+    <p>
+        Lorem ipsum
+        In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate
+        the visual form of a document or a typeface without relying on meaningful content.
+        Lorem ipsum may be used as a placeholder before final copy is available
+    </p>
+</article>
+
+<a href="/">Back</a>
+</body>
+</html>
